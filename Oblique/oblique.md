b194da9344ca530cdd04d861c248db10018d0dcc
The XOR <1h3x0r@gmail.com>
2025-04-22 19:54:26 +0200

    Ullalla'

---
+++ Oblique/oblique.md
@@ -0,0 +1,134 @@
+Remove specifics and convert to ambiguities 
+Think of the radio
+Don't be frightened of clichés
+What is the reality of the situation?
+Are there sections? Consider transitions
+Turn it upside down
+Allow an easement (an easement is the abandonment of a stricture)
+Simple subtraction
+Remove specifics and convert to ambiguities
+Go slowly all the way round the outside
+A line has two sides 
+Infinitesimal gradations
+Make an exhaustive list of everything you might do and do the last thing on the list
+Into the impossible
+Ask people to work against their better judgment
+Take away the elements in order of apparent non-importance
+Change instrument roles
+Accretion
+Disconnect from desire
+Emphasize repetitions
+Don't be afraid of things because they're easy to do Is there something missing?
+Don't be frightened to display your talents
+Breathe more deeply
+Honor thy error as a hidden intention
+Only one element of each kind
+Use unqualified people
+How would you have done it?
+Emphasize differences
+Do nothing for as long as possible
+Bridges -build
+Bridges -burn
+Water
+You don't have to be ashamed of using your own ideas
+Tidy up
+Do the words need changing?
+Ask your body
+Make a sudden, destructive unpredictable action; incorporate
+Consult other sources
+Use an unacceptable color
+Humanize something free of error
+Use filters 
+Balance the consistency principle with the inconsistency principle
+Fill every beat with something
+Discard an axiom
+What wouldn't you do?
+Decorate, decorate
+Listen to the quiet voice
+Is it finished?
+Put in earplugs
+Give the game away Reverse
+Abandon normal instruments
+Use fewer notes
+Repetition is a form of change
+Give way to your worst impulse
+Trust in the you of now
+What would your closest friend do?
+Distorting time
+Make a blank valuable by putting it in an exquisite frame
+The inconsistency principle
+Ghost echoes
+You can only make one dot at a time
+Just carry on
+(Organic) machinery
+Don't break the silence
+Discover the recipes you are using and abandon them
+Cascades
+Courage!
+What mistakes did you make last time? You are an engineer
+Consider different fading systems
+Mute and continue
+It is quite possible (after all)
+Don't stress one thing more than another
+Remove ambiguities and convert to specifics
+Look at the order in which you do things
+Go outside. Shut the door.
+Do we need holes?
+Cluster analysis Work at a different speed
+Do something boring
+Define an area as ‘safe’ and use it as an anchor
+Overtly resist change
+Accept advice
+Look closely at the most embarrassing details and amplify them
+Mechanicalize something idiosyncratic
+Emphasize the flaws
+Remember those quiet evenings
+Take a break The tape is now the music
+Short circuit (example; a man eating peas with the idea that they will improve his virility shovels them straight into his lap)
+Use an old idea
+Destroy -nothing
+Destroy -the most important thing
+Change nothing and continue with immaculate consistency
+Imagine the music as a moving chain or caterpillar
+Intentions -credibility
+Intentions -nobility of
+Intentions -humility of
+Imagine the music as a set of disconnected events
+Imagine the piece as a set of disconnected events
+What are you really thinking about just now? Incorporate.
+Children's voices -speaking
+Children's voices -singing
+Assemble some of the instruments in a group and treat the group
+Shut the door and listen from outside
+Is the tuning appropriate?
+Look at a very small object, look at its centre
+Feedback recordings into an acoustic situation
+Towards the insignificant
+Simply a matter of work
+Not building a wall but making a brick
+Revaluation (a warm feeling) Disciplined self-indulgence
+The most important thing is the thing most easily forgotten
+Idiot glee
+Be extravagant
+State the problem in words as clearly as possible
+Always first steps
+Question the heroic approach
+Always give yourself credit for having more than personality
+Faced with a choice, do both
+Tape your mouth Twist the spine
+Get your neck massaged
+Do the washing up
+Convert a melodic element into a rhythmic element
+Spectrum analysis
+Lowest common denominator check -single beat
+Lowest common denominator check -single note
+Lowest common denominator check -single riff
+Listen in total darkness, or in a very large room, very quietly
+Would anybody want it?
+Retrace your steps
+Go to an extreme, move back to a more comfortable place
+Once the search is in progress, something will be found
+Only a part, not the whole
+From nothing to more than nothing
+Be less critical more often
+[Blank Card - Think something]
---
+++ creativity.py
@@ -0,0 +1,223 @@
+"""
+Connessioni di connessione:
+   Pulsante gira la capa tra GND e GPIO4
+   Led Rosso (220 ohm) GPIO 13
+   Led Verde (220 ohm) GPIO 19
+   Led Blu (220 ohm) GPIO 26
+   Display ST7735:
+      Pin 1 = GND
+      Pin 2 = 3.3V
+      Pin 3  (SCK) = SCLK
+      Pin 4  (SDA) = MOSI
+      Pin 5  (RES) =  GPIO24
+      Pin 6  (RS) =  GPIO25
+      Pin 7  (CS) = CE0
+      Pin 8  (BKLIGHT) = GPIO21
+
+   Strip MAX7219:
+      Pin 1  (VCC) = 3.3V
+      Pin 2  (GND) = GND
+      Pin 3  (DIN) = MOSI
+      Pin 4  (CS) =  CE1
+      Pin 1  (CLK) = SCLK
+"""
+
+import RPi.GPIO as GPIO
+import time
+import random
+from PIL import Image, ImageDraw
+from luma.lcd.device import st7735
+from luma.core.interface.serial import spi, noop
+from luma.led_matrix.device      import max7219
+from luma.core.legacy import text
+from luma.core.render            import canvas
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.virtual import viewport
+
+GPIO.setmode(GPIO.BCM)
+st7735spi = spi(port=0, device=0, gpio_DC=25, gpio_RST=24,bus_speed_hz=24000000, transfer_size=4096)
+display = st7735(serial_interface=st7735spi, width=160, height=128, rotation=0, bgr=False)
+
+st7219spi = spi(port=0, device=1, gpio=noop())  #device 1 = CE1
+d7219 = max7219(st7219spi,  cascaded=4, block_orientation=90, rotate=2, blocks_arranged_in_reverse_order=1)
+d7219.contrast(1)
+
+BACKLIGHT_PIN = 21
+DICE_PIN = 4
+LED_BLU = 26
+LED_VERDE = 19
+LED_ROSSO = 13
+
+GPIO.setup(BACKLIGHT_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_BLU, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_VERDE, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_ROSSO, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(DICE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+text_file = open("Oblique/obliqui.txt", "r")
+lines = text_file.read().split('\n')
+text_file.close()
+lines = [line for line in lines if line.strip()]
+key_pressed = False
+led_time = 0
+curSentence = curTarot = -1
+
+def coloraLed():
+    global led_time
+    if curTarot >= 22:
+        now = time.time()
+        if now - led_time > 1:
+            led_time = now
+            GPIO.output(LED_BLU, GPIO.HIGH if random.randint(0,1) == 1 else GPIO.LOW)
+            GPIO.output(LED_VERDE, GPIO.HIGH if random.randint(0,1) == 1 else GPIO.LOW)
+            GPIO.output(LED_ROSSO, GPIO.HIGH if random.randint(0,1) == 1 else GPIO.LOW)
+
+def loadimg(n):
+    img = Image.open("Tarot/"+str(n)+".jpg").convert("RGB")
+    img = img.rotate(-90, expand=True)
+    return img.resize((display.width, display.height), Image.BICUBIC)
+
+def checkKey():
+    global key_pressed
+    if GPIO.input(DICE_PIN) == 0:
+        key_pressed = True
+    coloraLed()
+    return key_pressed
+
+def showSentence(tarot, oblique):
+    global curSentence, curTarot
+    if tarot != curTarot:
+        curTarot = tarot
+        if tarot >= 22:
+            GPIO.output(BACKLIGHT_PIN, GPIO.LOW)
+        else:
+            img = loadimg(tarot)
+            display.display(img)
+            GPIO.output(BACKLIGHT_PIN, GPIO.HIGH)
+
+    if curSentence != oblique:
+        curSentence = oblique
+        if oblique < len(lines):
+            msg = lines[oblique]
+            show_message_with_callback(d7219, "       " + msg, callback=checkKey)
+
+def repeatSentence():
+    global curSentence
+    if curSentence < len(lines):
+        show_message_with_callback(d7219, "       " + lines[curSentence], callback=checkKey)
+    else:
+        thexor()
+
+def print_message(device, message, font=proportional(SINCLAIR_FONT), fill="white"):
+    with canvas(device) as draw:
+        text(draw, (0, 0), message, font=font, fill=fill)
+
+def show_message_with_callback(device, message, font=proportional(SINCLAIR_FONT), fill="white", scroll_delay=0.08, callback=None):
+    global key_pressed
+    message = message + "              "
+    text_width = sum(len(font[ord(c)]) for c in message)
+    virt = viewport(device, width=text_width, height=device.height)
+    print_message(virt, message, font=font, fill=fill)
+
+    # Scroll 
+    for x in range(0, text_width-device.width):
+        virt.set_position((x, 0))
+        if callback:
+            callback(x)
+        if key_pressed:
+            break
+        if scroll_delay > 0:
+            time.sleep(scroll_delay)
+
+def animotion(buf, draw, destX, destY, speed=0.1, reverse = False):
+    if reverse:
+        curX = destX
+        curY = destY
+        while curY < 7:
+            set_pixel(d7219, buf, draw, curX, curY, False)
+            if speed > 0:
+                time.sleep(speed)
+            curY = curY + 1
+            set_pixel(d7219, buf, draw, curX, curY, True)
+            if checkKey():
+                return True
+
+        while curX < 31:
+            set_pixel(d7219, buf, draw, curX, curY, False)
+            if speed > 0:
+                time.sleep(speed)
+            curX = curX + 1
+            set_pixel(d7219, buf, draw, curX, curY, True)
+            if checkKey():
+                return True
+        set_pixel(d7219, buf, draw, curX, curY, False)
+
+    else:
+        curX = 31
+        curY = 7
+        while curX > destX:
+            set_pixel(d7219, buf, draw, curX, curY, True)
+            if speed > 0:
+                time.sleep(speed)
+            set_pixel(d7219, buf, draw, curX, curY, False)
+            curX = curX - 1
+            if checkKey():
+                return True
+
+        while curY > destY:
+            set_pixel(d7219, buf, draw, curX, curY, True)
+            if speed > 0:
+                time.sleep(speed)
+            set_pixel(d7219, buf, draw, curX, curY, False)
+            curY = curY - 1
+            if checkKey():
+                return True
+        set_pixel(d7219, buf, draw, curX, curY, True)
+    return False
+
+def set_pixel(device, buf, draw, x, y, on=True):
+    draw.point((x, y), fill="white" if on else "black")
+    device.display(buf)    # instantly pushes the whole image
+
+def thexor(speed=0.01):
+    buf = Image.new('1', (d7219.width, d7219.height))
+    draw = ImageDraw.Draw(buf)
+
+    coords=[ (0,2), (0,3), (0,4), (0,5), (1,3), (1,6), (2,3), (2,6), #t
+         (4,2), (4,3), (4,4), (4,5), (4,6), (5,4), (6,4), (7,5), (7,6), #h
+         (9,3), (9,4),(9,5), (10,2),(10,4),(10,6),(11,2),(11,4),(11,6),(12,3), #(12,6), #e
+         (15,0),(15,1),(15,5),(15,6),(16,2),(16,4),(17,3),(18,2),(18,4),(19,0),(19,1),(19,5),(19,6),  #X
+         (21,1),(21,2),(21,3),(21,4),(21,5),(22,0),(22,6),(23,0),(23,6),(24,0),(24,6),(25,1),(25,2),(25,3),(25,4),(25,5), #O
+         (27,0),(27,1),(27,2),(27,3),(27,4),(27,5),(27,6),(28,0),(28,3),(29,0),(29,3),(29,4),(30,0),(30,3),(30,5),(31,1),(31,2),(31,6) #R
+    ]
+    for t in coords:
+       if animotion(buf, draw, t[0], t[1], speed, False):
+          return True
+
+    coords.reverse()
+    for t in coords:
+       if animotion(buf, draw, t[0], t[1], speed, True):
+          return True
+
+    return False
+
+def blankSentence():
+    showSentence(22, len(lines))
+
+blankSentence()
+try:
+    while True:
+        if key_pressed:
+           key_pressed = False
+           if curTarot < 22:
+              blankSentence()
+           else:
+              random.seed()
+              showSentence(random.randint(0, 22), random.randint(0, len(lines)))
+        repeatSentence()
+        time.sleep(0.2)
+
+finally:
+    GPIO.output(BACKLIGHT_PIN, GPIO.LOW)
+    GPIO.cleanup()
+    print("Clean")
